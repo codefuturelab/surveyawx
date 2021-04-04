8296f6cc7dbc3a237894d80b30f497dad736ad2b
codefuturelab <codefuturelab@gmail.com>
2021-04-04 14:08:10 +0300

    fix

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,9 +13,9 @@ class Example extends React.Component {
     return (
       <Menu>
         <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+        <a id="about" className="menu-item" href="/about">Components</a>
+        <a id="contact" className="menu-item" href="/contact">Surveys</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Jobs</a>
       </Menu>
     );
   }
